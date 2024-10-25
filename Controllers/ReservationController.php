c7507e102f4f5be7be6aefac5e47ac0f75e86508
Jeremy Hoarau <jeremyhoarau44@gmail.com>
2024-10-25 16:28:09 +0400

    MàJ du controleur de reservation

--- Controllers/ReservationController.php
+++ Controllers/ReservationController.php
@@ -19,41 +19,45 @@
             // if (isset($_SESSION['status']) && $_SESSION['status'] == true && isset($_SESSION['id'])) {
                 $this->view('Reservation/Reservation');
             // } else {
-                // header("Location: index.php?url=user/connexion&msg=Veuillez vous connecter.");
+                // header("Location: index.php?url=user/connection&msg=Veuillez vous connecter.");
             // }
         }
 
-        public function recapitulatif() {
+        /* public function recapitulatif() {
             $this->view('reservation/recapitulatif');
-        }
+        } */
 
         public function store() {
 
-            // if (isset($_POST['submit'])) {
+            if (isset($_POST['submit'])) {
 
-            //     try {
-            //         $iduser = $_SESSION['id'];
+                try {
+                    $iduser = $_SESSION['id'];
 
-            //         $reservation = [
-            //             $_POST['date'],
-            //             $_POST['time-start'],
-            //             $_POST['time-end'],
-            //             $prixTotal,
-            //             $iduser
-            //         ];
+                    $reservation = [
+                        $_POST['date'],
+                        $_POST['date'],
+                        $_POST['time-start'],
+                        $_POST['time-end'],
+                        // $prixTotal,
+                        $iduser
+                    ];
 
-            //         $this->reservationModel->insertReservation($reservation);
-            //         $this->reservationModel->insertSalleReservee($salle, $this->reservationModel->getIdReservation($iduser));
-            //         $this->reservationModel->insertEquipementReservee($equipement, $this->reservationModel->getIdReservation($iduser));
-            //         $this->reservationModel->insertServiceReservee($service, $this->reservationModel->getIdReservation($iduser));
+                    $reserved_car = [
+                        "reservationId" => $this->reservationModel->getReservationId($iduser),
+                        "carId" => ""
+                    ];
 
-            //         header('Location: index.php?msg=Votre réservation a bien été enregistrée.');
-            //         exit;
-            //     } catch(\Exception $e) {
-            //         echo $e;
-            //     }
+                    $this->reservationModel->insertReservation($reservation);
+                    $this->reservationModel->insertCarReserved($reserved_car);
 
-            // }
+                    header('Location: index.php?msg=Votre réservation a bien été enregistrée.');
+                    exit;
+                } catch(\Exception $e) {
+                    echo $e;
+                }
+
+            }
         }
 
         public function get() {
