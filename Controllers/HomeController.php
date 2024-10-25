088020644813d6b741e3bd2c4183a59455ef8e6e
Jeremy Hoarau <jeremyhoarau44@gmail.com>
2024-10-25 11:28:06 +0400

    Ajout du controlleur de la page d'accueil

---
+++ Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace Controllers;
+
+    session_start();
+
+    class HomeController extends Controller
+    {
+
+        public function index() {
+            $this->view('Home');
+        }
+
+        // public function mention() {
+        //     $this->view("mentionLegale");
+        // }
+
+        public function error() {
+            $this->view("Error");
+        }
+    }
